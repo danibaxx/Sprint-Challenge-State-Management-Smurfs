138c3dd2bc3fc651f33b8a5732280d132a8a27e0
Danielle B <danibaxx@hotmail.com>
2019-10-27 18:47:44 -0600

    added axios call to bring data in from API/locally, rendering array data

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+
 import SmurfForm from './smurf/SmurfForm';
 
 import "./App.css";
-
 
 const App = () =>  {
   const [smurf, setSmurf] = useState([]);
 
   useEffect(() => {
     setSmurf([])
-    axios.get()
-  })
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log("Response", response.data)
+        setSmurf(response.data)
+      })
+      .catch(error => {
+        console.log('GET ERROR', error)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div>
-      <SmurfForm />
+      <SmurfForm smurf={smurf} />
     </div>
   );
 }
